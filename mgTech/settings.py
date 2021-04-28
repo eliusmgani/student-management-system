4a7542930bd64b915e58b17cd236842c9ba62de4
EliusMgani <eliasmgani@gmail.com>
2021-04-28 15:14:31 +0300

    pkg-resurces == 0.0.0 removed

--- mgTech/settings.py
+++ mgTech/settings.py
@@ -4,6 +4,10 @@ import django_heroku
 import dj_database_url
 import django_heroku
 import dj_database_url
+import django_heroku
+import dj_database_url
+import django_heroku
+import dj_database_url
 """
 Django settings for mgTech project.
 
